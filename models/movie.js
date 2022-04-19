e41e730e7224adfc319c69e7d09f46979d27e2d2
ale-lit <zeldor@ya.ru>
2022-04-19 14:37:48 +0300

    feat: add scheme for user & movie

---
+++ models/movie.js
@@ -0,0 +1,74 @@
+const mongoose = require('mongoose');
+const urlRegexpPattern = require('../regexp');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegexpPattern.test(v);
+      },
+      message: (props) => `${props.value} не корректный url!`,
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegexpPattern.test(v);
+      },
+      message: (props) => `${props.value} не корректный url!`,
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(v) {
+        return urlRegexpPattern.test(v);
+      },
+      message: (props) => `${props.value} не корректный url!`,
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);
