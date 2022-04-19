534cf2e744f22d62b0ca01ff1fa739f42e119e78
ale-lit <zeldor@ya.ru>
2022-04-19 16:30:33 +0300

    feat: add authorization

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const { celebrate, Joi, errors } = require('celebrate');
 const usersRouter = require('./routes/users');
 const moviesRouter = require('./routes/movies');
+const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const NotFoundError = require('./errors/not-found-err');
 
-const app = express();
+const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/filmsdb', {
   useNewUrlParser: true,
@@ -13,11 +17,53 @@ mongoose.connect('mongodb://localhost:27017/filmsdb', {
   useFindAndModify: false,
 });
 
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.use(requestLogger);
+
+app.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
 
 app.use(auth);
 
 app.use(usersRouter);
 app.use(moviesRouter);
+app.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемый ресурс не найден.'));
+});
 
-app.listen(3000);
+app.use(errorLogger);
+
+app.use(errors());
+
+// eslint-disable-next-line no-unused-vars
+app.use((err, req, res, next) => {
+  // если у ошибки нет статуса, выставляем 500
+  const { statusCode = 500, message } = err;
+
+  res
+    .status(statusCode)
+    .send({
+      // проверяем статус и выставляем сообщение в зависимости от него
+      message: statusCode === 500
+        ? 'На сервере произошла ошибка'
+        : message,
+    });
+});
+
+app.listen(PORT);
--- controllers/users.js
+++ controllers/users.js
@@ -9,14 +9,41 @@ const DefaultError = require('../errors/default-err');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
-// module.exports.createUser = (req, res, next) => {
-//   const {
-//     name,
-//     about,
-//     avatar,
-//     email,
-//     password,
-//   } = req.body;
+module.exports.createUser = (req, res, next) => {
+  const { email, password, name } = req.body;
+
+  User.find({ email })
+    .then((result) => {
+      if (result.length === 0) {
+        bcrypt
+          .hash(password, 10)
+          .then((hash) => User.create({
+            email,
+            name,
+            password: hash,
+          }))
+          .then((user) => res.status(201).send({
+            name: user.name,
+            email: user.email,
+            _id: user._id,
+          }))
+          .catch((err) => {
+            if (err.name === 'ValidationError') {
+              throw new BadRequestError(
+                'Переданы некорректные данные при создании пользователя.',
+              );
+            }
+            throw new DefaultError('Произошла ошибка');
+          })
+          .catch(next);
+      } else {
+        throw new ConflictError(
+          `Пользователь с адресом электронной почты ${email} уже существует!`,
+        );
+      }
+    })
+    .catch(next);
+};
 
 module.exports.getUserMe = (req, res, next) => {
   User.findById(req.user._id)
@@ -28,7 +55,9 @@ module.exports.getUserMe = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'CastError') {
-        throw new BadRequestError('Переданы некорректные данные вместо id пользователя.');
+        throw new BadRequestError(
+          'Переданы некорректные данные вместо id пользователя.',
+        );
       }
       throw err;
     })
@@ -38,7 +67,11 @@ module.exports.getUserMe = (req, res, next) => {
 module.exports.updateUser = (req, res, next) => {
   const { email, name } = req.body;
 
-  User.findByIdAndUpdate(req.user._id, { email, name }, { new: true, runValidators: true })
+  User.findByIdAndUpdate(
+    req.user._id,
+    { email, name },
+    { new: true, runValidators: true },
+  )
     .then((user) => {
       if (!user) {
         throw new NotFoundError('Пользователь по указанному id не найден.');
@@ -47,31 +80,34 @@ module.exports.updateUser = (req, res, next) => {
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
-        throw new BadRequestError('Переданы некорректные данные при обновлении профиля.');
+        throw new BadRequestError(
+          'Переданы некорректные данные при обновлении профиля.',
+        );
       }
       if (err.name === 'CastError') {
-        throw new BadRequestError('Переданы некорректные данные вместо id пользователя.');
+        throw new BadRequestError(
+          'Переданы некорректные данные вместо id пользователя.',
+        );
       }
       throw err;
     })
     .catch(next);
 };
 
-// module.exports.login = (req, res, next) => {
-//   const { email, password } = req.body;
+module.exports.login = (req, res, next) => {
+  const { email, password } = req.body;
 
-//   return User.findUserByCredentials(email, password)
-//     .then((user) => {
-//       const token = jwt.sign(
-//         { _id: user._id },
-//         NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
-//         { expiresIn: '7d' },
-//       );
-//       res.send({ token: `Bearer ${token}` });
-//     })
-//     .catch(() => {
-//       // ошибка аутентификации
-//       throw new UnauthorizedError('Передан неверный логин или пароль.');
-//     })
-//     .catch(next);
-// };
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'some-secret-key',
+        { expiresIn: '7d' },
+      );
+      res.send({ token: `Bearer ${token}` });
+    })
+    .catch(() => {
+      throw new UnauthorizedError('Передан неверный логин или пароль.');
+    })
+    .catch(next);
+};
