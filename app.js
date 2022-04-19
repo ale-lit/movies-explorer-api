195740c929660ef802a07d9ac17bd6ffdeda6407
ale-lit <zeldor@ya.ru>
2022-04-19 14:52:52 +0300

    feat: add users route

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const usersRouter = require('./routes/users');
+const moviesRouter = require('./routes/movies');
 
 const app = express();
 
-// подключаемся к серверу mongo
 mongoose.connect('mongodb://localhost:27017/filmsdb', {
   useNewUrlParser: true,
   useCreateIndex: true,
   useFindAndModify: false,
 });
 
-// подключаем мидлвары, роуты и всё остальное...
+app.use(usersRouter);
+app.use(moviesRouter);
 
 app.listen(3000);
