9cc999645ae9f4041f409067bc81fd2f69e55d83
ale-lit <zeldor@ya.ru>
2022-04-19 21:01:58 +0300

    refact: optimize code

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const moviesRouter = require('./routes/movies');
 const { createUser, login } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
+const allErrors = require('./middlewares/all-errors');
 const NotFoundError = require('./errors/not-found-err');
 
 const { PORT = 3000 } = process.env;
@@ -54,19 +55,6 @@ app.use(errorLogger);
 
 app.use(errors());
 
-// eslint-disable-next-line no-unused-vars
-app.use((err, req, res, next) => {
-  // если у ошибки нет статуса, выставляем 500
-  const { statusCode = 500, message } = err;
-
-  res
-    .status(statusCode)
-    .send({
-      // проверяем статус и выставляем сообщение в зависимости от него
-      message: statusCode === 500
-        ? 'На сервере произошла ошибка.'
-        : message,
-    });
-});
+app.use(allErrors);
 
 app.listen(PORT);
