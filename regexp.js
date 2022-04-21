aff3a661b5d5742422852ac53457c6d200e856cb
ale-lit <zeldor@ya.ru>
2022-04-21 10:26:13 +0300

    refact: fix regexp

--- regexp.js
+++ regexp.js
@@ -1,4 +1,3 @@
-// eslint-disable-next-line no-useless-escape
-const urlRegexpPattern = /^((?:https?\:)?(?:\/{2})?)?((?:[\w\d-_]{1,64})\.(?:[\w\d-_\.]{2,64}))(\:\d{2,6})?((?:\/|\?|#|&){1}(?:[\w\d\S]+)?)?$/;
+const urlRegexpPattern = /^((?:https?:)?(?:\/{2})?)?((?:[\w\d\-_]{1,64})\.(?:[\w\d\-_.]{2,64}))(:\d{2,6})?((?:\/|\?|#|&){1}(?:[\w\d\S]+)?)?$/;
 
 module.exports = urlRegexpPattern;
